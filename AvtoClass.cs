4d4f04d7b5835dabc8a6113773a671a4a76141f5
Snejin <81646732+Snejin@users.noreply.github.com>
2023-10-23 16:12:43 +0300

    Add files via upload

---
+++ AvtoClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Car
+{
+    private double fuelTank; // объем бака
+    private double fuelConsumption; // расход топлива на 1 км
+
+    public Car(double fuelTank, double fuelConsumption)
+    {
+        this.fuelTank = fuelTank;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void FillFuelTank(double fuelVolume)
+    {
+        fuelTank += fuelVolume;
+        Console.WriteLine($"Бак заправлен. Текущий объем топлива: {fuelTank} л");
+    }
+
+    public void Drive()
+    {
+        Random random = new Random();
+
+        double distance = random.Next(1, 50); // по расстоянию я не уверен что так нормально в случае чего поменять надо будет
+        
+        double fuelNeeded = fuelConsumption * distance; // расчет необходимого топлива на наш заезд
+
+        if (fuelNeeded <= fuelTank)
+        {
+            fuelTank -= fuelNeeded;
+            Console.WriteLine($"Проехали {distance} км. Остаток топлива: {fuelTank} л");
+        }
+        else
+        {
+            Console.WriteLine($"Недостаточно топлива для поездки на {distance} км");
+
+            Console.Write("Хотите заправиться? (да/нет): ");
+            string userChoice = Console.ReadLine();
+
+            if (userChoice.ToLower() == "да")
+            {
+                Console.Write("Введите объем заправляемого топлива: "); //надо сделать сравнение с объемом бака
+                double fuelVolume = Convert.ToDouble(Console.ReadLine());
+                FillFuelTank(fuelVolume);
+            }
+        }
+    }
+
+    public void Brake()
+    {
+        Console.WriteLine("Торможение..."); // я не понимаю зачем оно нам но оно нам надо. с разгоном так же
+    }
+
+    public void Accelerate()
+    {
+        Console.WriteLine("Разгон...");
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите объем бака: ");
+        double fuelTankVolume = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите расход топлива на 1 км: ");
+        double fuelConsumption = Convert.ToDouble(Console.ReadLine());
+
+        Car car = new Car(fuelTankVolume, fuelConsumption);
+
+        Console.Write("Введите объем заправляемого топлива: ");
+        double initialFuelVolume = Convert.ToDouble(Console.ReadLine());
+        car.FillFuelTank(initialFuelVolume);
+
+        while (true)
+        {
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("1 - Поехать");
+            Console.WriteLine("2 - Торможение");
+            Console.WriteLine("3 - Разгон");
+            Console.WriteLine("0 - Выход");
+            Console.Write("Ваш выбор: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    car.Drive();
+                    break;
+                case 2:
+                    car.Brake();
+                    break;
+                case 3:
+                    car.Accelerate();
+                    break;
+                case 0:
+                    return;
+                default:
+                    Console.WriteLine("Некорректный выбор");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
